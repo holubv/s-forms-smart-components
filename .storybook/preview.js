fb27c0ada1b6426d5dcaa5a711128fd7fe6ef716
Max Chopart <max.chopart@gmail.com>
2022-03-16 10:49:19 +0100

    [New #12] Migration to Microbundle

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
-import 's-forms/css/s-forms.min.css';
 import '../src/styles/components.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 's-forms/css/s-forms.min.css';
 import "intelligent-tree-select/lib/styles.css"
 
 export const parameters = {
