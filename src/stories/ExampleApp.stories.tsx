c9d8358ed6e4dd11405c5bb03495362e556275ed
Max Chopart <max.chopart@gmail.com>
2022-03-16 10:34:23 +0100

    [New #12] Set up Prettier

--- src/stories/ExampleApp.stories.tsx
+++ src/stories/ExampleApp.stories.tsx
@@ -1,29 +1,28 @@
-import React, {useRef, useState} from 'react';
-import SForms, {Constants} from 's-forms';
+import React, { useRef, useState } from "react";
+import SForms, { Constants } from "s-forms";
 import SmartComponents from "../SmartComponents";
-import exampleForm from './assets/example_form.json';
-import exampleFormTC from './assets/example_turisticky_cil.json';
-import possibleValues from './assets/possibleValues.json';
-import {ComponentMeta, ComponentStory} from "@storybook/react";
+import exampleForm from "./assets/example_form.json";
+import exampleFormTC from "./assets/example_turisticky_cil.json";
+import possibleValues from "./assets/possibleValues.json";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 const componentMapping = SmartComponents.getComponentMapping();
 
 const modalProps = {
-  onHide: () => {
-  },
+  onHide: () => {},
   show: true,
-  title: 'Title'
+  title: "Title",
 };
 
 const options = {
   i18n: {
-    'wizard.next': 'Next',
-    'wizard.previous': 'Previous',
-    'section.expand': 'Expand',
-    'section.collapse': 'Collapse'
+    "wizard.next": "Next",
+    "wizard.previous": "Previous",
+    "section.expand": "Expand",
+    "section.collapse": "Collapse",
   },
   intl: {
-    locale: 'cs'
+    locale: "cs",
   },
   modalView: false,
   modalProps,
@@ -32,19 +31,32 @@ const options = {
   enableForwardSkip: true,
   startingStep: 1,
   users: [
-    {id: "http://fel.cvut.cz/people/max-chopart", label: "Max Chopart"},
-    {id: "http://fel.cvut.cz/people/miroslav-blasko", label: "Miroslav Blasko"}],
+    { id: "http://fel.cvut.cz/people/max-chopart", label: "Max Chopart" },
+    {
+      id: "http://fel.cvut.cz/people/miroslav-blasko",
+      label: "Miroslav Blasko",
+    },
+  ],
   currentUser: "http://fel.cvut.cz/people/max-chopart",
   icons: [
-    {id: Constants.ICONS.QUESTION_HELP, behavior:  Constants.ICON_BEHAVIOR.ENABLE},
-    {id: Constants.ICONS.QUESTION_COMMENTS, behavior: Constants.ICON_BEHAVIOR.ON_HOVER},
-    {id: Constants.ICONS.QUESTION_LINK, behavior:  Constants.ICON_BEHAVIOR.ON_HOVER},
-  ]
+    {
+      id: Constants.ICONS.QUESTION_HELP,
+      behavior: Constants.ICON_BEHAVIOR.ENABLE,
+    },
+    {
+      id: Constants.ICONS.QUESTION_COMMENTS,
+      behavior: Constants.ICON_BEHAVIOR.ON_HOVER,
+    },
+    {
+      id: Constants.ICONS.QUESTION_LINK,
+      behavior: Constants.ICON_BEHAVIOR.ON_HOVER,
+    },
+  ],
 };
 
 export default {
-  title: 'SForms Smart Component',
-  component: SForms
+  title: "SForms Smart Component",
+  component: SForms,
 } as ComponentMeta<typeof SForms>;
 
 const Template: ComponentStory<typeof SForms> = (args) => {
@@ -53,21 +65,27 @@ const Template: ComponentStory<typeof SForms> = (args) => {
   const refForm = useRef();
 
   const fetchTypeAheadValues = (query) => {
-    return new Promise((resolve) => setTimeout(() => resolve(possibleValues), 1500));
+    return new Promise((resolve) =>
+      setTimeout(() => resolve(possibleValues), 1500)
+    );
   };
 
-
-    return (
-        <SForms options={options} componentMapping={componentMapping} form={selectedForm} {...args} />
-    );
-}
+  return (
+    <SForms
+      options={options}
+      componentMapping={componentMapping}
+      form={selectedForm}
+      {...args}
+    />
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
-  options: options
-}
+  options: options,
+};
 export const AdvancedComponents = Template.bind({});
 AdvancedComponents.args = {
   options: options,
-  form: exampleFormTC
-}
+  form: exampleFormTC,
+};
