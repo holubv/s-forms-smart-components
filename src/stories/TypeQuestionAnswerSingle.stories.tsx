00a2df98bd246b9e2cd2abf13bce7c306e47dd66
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2023-07-22 11:13:47 +0200

    [New #28] Single and multi select options stories divided

--- src/stories/TypeQuestionAnswerSingle.stories.tsx
+++ src/stories/TypeQuestionAnswerSingle.stories.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import SForms, { Constants, IntlContextProvider } from "@kbss-cvut/s-forms";
 import SmartComponents from "../SmartComponents";
 import possibleValues from "./assets/possibleValues.json";
-import testData from "./assets/type_question_answer.json";
+import singleSelectData from "./assets/type_question_answer_single.json";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 const componentMapping = SmartComponents.getComponentMapping();
@@ -54,7 +54,7 @@ const options = {
 };
 
 export default {
-  title: "Components/TypeQuestionAnswer/Multi",
+  title: "Components/TypeQuestionAnswer/SingleSelect",
   component: SForms,
 } as ComponentMeta<typeof SForms>;
 
@@ -72,7 +72,7 @@ const Template: ComponentStory<typeof SForms> = (args) => {
       <SForms
         options={options}
         ref={refForm}
-        form={testData}
+        form={singleSelectData}
         fetchTypeAheadValues={fetchTypeAheadValues}
         componentMapRules={componentMapping}
         {...args}
